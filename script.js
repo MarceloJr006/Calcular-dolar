e31810aa5f1202ac7f41562c4e7b5e81d684147c
Marcelo Aparecido Malvezi Junior <62224726+MarceloJr006@users.noreply.github.com>
2023-04-17 00:57:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let dolar = 0;
+
+fetch('https://api.hgbrasil.com/finance?format=json-cors&key=sua_chave_api')
+  .then(response => response.json())
+  .then(data => {
+    dolar = data.results.currencies.USD.buy;
+    document.getElementById('dolar').innerHTML = dolar.toFixed(2);
+  })
+  .catch(error => console.error(error));
+
+document.getElementById('btn-convert').addEventListener('click', function() {
+  const valorReais = document.getElementById('valor-reais').value;
+  const valorDolar = valorReais / dolar;
+  document.getElementById('valor-dolar').innerHTML = valorDolar.toFixed(2);
+});
