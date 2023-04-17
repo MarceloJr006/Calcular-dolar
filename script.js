52af5ffc59107771de3e1c6f791910bee5f64ed8
Marcelo Aparecido Malvezi Junior <62224726+MarceloJr006@users.noreply.github.com>
2023-04-17 01:00:58 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let dolar = 0;
 
-fetch('https://api.hgbrasil.com/finance?format=json-cors&key=sua_chave_api')
+fetch('https://api.hgbrasil.com/finance?format=json-cors&key=9dbb8d06')
   .then(response => response.json())
   .then(data => {
     dolar = data.results.currencies.USD.buy;
