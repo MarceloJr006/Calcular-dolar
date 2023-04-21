f314b03dacb0e45328fc8e3de74f474fdc89d077
Marcelo Aparecido Malvezi Junior <62224726+MarceloJr006@users.noreply.github.com>
2023-04-20 21:47:56 -0300

    Atualização de api

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let dolar = 0;
 
-fetch('https://api.hgbrasil.com/finance?format=json-cors&key=9dbb8d06')
+fetch('https://api.hgbrasil.com/finance?format=json-cors&key=2b375e9f')
   .then(response => response.json())
   .then(data => {
     dolar = data.results.currencies.USD.buy;
